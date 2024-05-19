561e574aee9b981635c60e20011f0a3b8ac22f1a
Yasir Adnan <yasiradnan2022@gmail.com>
2024-05-19 22:45:24 +0600

    Create Vowel Counting.cpp

---
+++ Vowel Counting.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<bool> isPrime(1000, 1);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // CODE HERE
+    string s;
+    cin>>s;
+    int i = 0,ans = 0;
+    transform(s.begin(), s.end(), s.begin(), ::tolower); 
+    while(i<s.size()){
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') ans++;
+        i++;
+    }
+    cout<<ans;
+    return 0;
+}
+
+
+// YASIR ADNAN
